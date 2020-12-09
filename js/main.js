af31d3ce2a0009232a9e98c0811e52ba73b8b0bf
RCdexterous <rcdexterous3232@gmail.com>
2020-12-09 08:24:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -87,24 +87,24 @@
         CountDown
     --------------------*/
     // For demo preview
-    var today = new Date();
-    var dd = String(3).padStart(2, '0');
-    var mm = String(0).padStart(2, '0'); //January is 0!
-    var yyyy = 2021;
-
-    if(mm == 12) {
-        mm = '01';
-        yyyy = yyyy + 1;
-    } else {
-        mm = parseInt(mm) + 1;
-        mm = String(mm).padStart(2, '0');
-    }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
+//    var today = new Date();
+//    var dd = String(3).padStart(2, '0');
+//    var mm = String(0).padStart(2, '0'); //January is 0!
+//    var yyyy = 2021;
+//
+//    if(mm == 12) {
+//        mm = '01';
+//        yyyy = yyyy;
+//    } else {
+//        mm = parseInt(mm) + 1;
+//        mm = String(mm).padStart(2, '0');
+//    }
+//    var timerdate = mm + '/' + dd + '/' + yyyy;
     // For demo preview end
     
 
     // Use this for real timer date
-    /*  var timerdate = "2020/01/01"; */
+      var timerdate = "2021/01/03 9:00:00"; 
 
 	$("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
